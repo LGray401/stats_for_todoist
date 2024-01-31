b6bc721f5cd711247a18e72d8b859b0e5cc462f6
Ludwig <ludwig.roeber@gmx.de>
2024-01-31 15:43:33 +0100

    changed to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,12 @@ import MainPage from './components/MainPage/MainPage';
 function App() {
   return (
     <AccessTokenProvider>
-      <Router basename={process.env.PUBLIC_URL}>
+
         <Routes>
           <Route path="/oauth-redirect" element={<OAuthRedirectHandler />} />
           <Route path="/" element={<MainPage />} />
         </Routes>
-      </Router>
+
     </AccessTokenProvider>
   );
 }
